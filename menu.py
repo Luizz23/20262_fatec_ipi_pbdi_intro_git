ac6c43d86d6a1bbc46425bbce37e567fa28f6f89
Luiz Fabiano <fabianop1910@gmail.com>
2026-08-18 21:37:53 -0300

    feat(ex-01-git): implementa a opcao multiplicar

--- menu.py
+++ menu.py
@@ -7,3 +7,5 @@ def menu (opcao):
          return print(f'{3} +  {5} = {soma(3,5)}')
     if opcao == "2":
         return print(f'{2} - {1} = {subtrair(2,1)}')
+     if opcao == "3":
+       return print(f'{6} * {5} = {multiplicar(6,5)}')
