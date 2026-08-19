bf17531960de81d4e66c53f828174cf38065c854
Luiz Fabiano <fabianop1910@gmail.com>
2026-08-18 21:39:43 -0300

    feat(ex-01-git): implementa a opcao divisao

--- menu.py
+++ menu.py
@@ -7,5 +7,7 @@ def menu (opcao):
          return print(f'{3} +  {5} = {soma(3,5)}')
     if opcao == "2":
         return print(f'{2} - {1} = {subtrair(2,1)}')
-     if opcao == "3":
+    if opcao == "3":
        return print(f'{6} * {5} = {multiplicar(6,5)}')
+    if opcao == "4":
+       return print(f'{10} / {2} = {dividir(10,2)}')
