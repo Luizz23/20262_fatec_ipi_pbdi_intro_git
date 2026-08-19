d41facc75b77bce88239686cc7e7b2ba2d1c2074
Luiz Fabiano <fabianop1910@gmail.com>
2026-08-18 21:36:37 -0300

    feat(ex-01-git): implementa a opcao subtrair

--- menu.py
+++ menu.py
@@ -4,4 +4,6 @@ opcao = input(("Escolha a opção do menu:"))
 
 def menu (opcao):
     if opcao == "1":
-         return print(f'{3} +  {5} = {somar(3,5)}')
+         return print(f'{3} +  {5} = {soma(3,5)}')
+    if opcao == "2":
+        return print(f'{2} - {1} = {subtrair(2,1)}')
